aadecd5f972526b4b32cebf8944e8632cc7c4766
oussemajebali <oussema.jbali@esprit.tn>
2024-05-28 00:04:49 +0100

    first commit

---
+++ src/main/java/com/example/universitymanagement/UniversitymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitymanagementApplication.class, args);
+	}
+
+}
